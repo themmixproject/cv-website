4cf896e63ca3ba670cc90ba7dbff8877b77229cf
themmixproject <steven.devalk@gmail.com>
2022-12-29 00:35:09 +0100

    Added container to vertically center welcome text, added base styling to welcome header

--- src/components/ProfileContent.vue
+++ src/components/ProfileContent.vue
@@ -5,6 +5,33 @@
             src="https://via.placeholder.com/500png?text=Profile+Picture"
             alt="Profile Picture"
         />
-        <h1>Hi, I'm Steven</h1>
+        <div id="welcome-header-container">
+            <h1>Hi, I'm Steven</h1>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+#welcome-container {
+    margin: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+
+    border: 2px solid black;
+    padding: 5px;
+
+    #welcome-header-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        h1 {
+            font-size: 2em;
+        }
+    }
+    #profile-image {
+        width: 10em;
+    }
+}
+</style>
