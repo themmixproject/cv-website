3ed8b653264906311bbb31c40ee1121d505242df
themmixproject <steven.devalk@gmail.com>
2022-12-29 00:17:29 +0100

    Added placeholder profile photo

--- src/components/ProfileContent.vue
+++ src/components/ProfileContent.vue
@@ -1,5 +1,10 @@
 <template>
     <div id="welcome-container">
+        <img
+            id="profile-image"
+            src="https://via.placeholder.com/500png?text=Profile+Picture"
+            alt="Profile Picture"
+        />
         <h1>Hi, I'm Steven</h1>
     </div>
 </template>
