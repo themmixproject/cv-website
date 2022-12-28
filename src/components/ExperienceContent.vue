bccdb9ff066d023ab6f3a3f8dbfebefeae5fc8ac
themmixproject <steven.devalk@gmail.com>
2022-12-29 00:46:29 +0100

    Added ExperienceContent component

---
+++ src/components/ExperienceContent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="experience-container">
+        <div>
+            <h2 class="experience-title">
+                <slot name="experience-name"></slot>
+            </h2>
+        </div>
+        <div>
+            <slot name="experience-company"></slot>
+        </div>
+        <div>
+            <slot name="experience-location"></slot>
+        </div>
+        <div>
+            <slot name="experience-duration"></slot>
+        </div>
+        <hr/>
+    </div>
+</template>
